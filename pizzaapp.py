4a4a94d9cae319940357ff9581e70704f91590fe
jinolini <Jin.han@stolav.no>
2025-09-05 13:04:11 +0200

    Update pizzaapp.py

--- pizzaapp.py
+++ pizzaapp.py
@@ -83,10 +83,10 @@ else:
 
 # --- Layout ---
 st.header("Ingridienser")
-st.write(f"**Mel:** {total_flour:.1f} g")
-st.write(f"**Vann:** {total_water:.1f} g")
-st.write(f"**Salt:** {total_salt:.1f} g")
-st.write(f"**Gjær:** {total_yeast:.1f} g")
+st.write(f"**Mel:** {total_flour:.1f} g ({100:.1f}%)")
+st.write(f"**Vann:** {total_water:.1f} g ({(hydration):.1f}%)")
+st.write(f"**Salt:** {total_salt:.1f} g ({(salt):.1f}%)")
+st.write(f"**Gjær:** {total_yeast:.1f} g ({(yeast):.2f}%)")
 
 # --- Gjæring section for standard recipe ---
 if st.session_state.recipe_mode == "standard":
